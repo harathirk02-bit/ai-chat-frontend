b7e8adabefe20b00a3cd469768dfe5ad0ad05215
K Harathi <harathirk02@gmail.com>
2026-06-01 14:39:59 +0530

    fixed chatbot frontend connection

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -9,47 +9,108 @@ function Chatbot() {
 
   const askQuestion = async () => {
 
+    const token = localStorage.getItem("token");
+
+    if(!token){
+
+      alert("Please Login First");
+
+      return;
+
+    }
+
     try {
 
-      const response = await axios.post(
+      const response = await axios.get(
+
         "https://ai-chat-backend-wtaf.onrender.com/chatbot",
+
         {
-          question
+
+          params:{
+
+            question:question
+
+          },
+
+          headers:{
+
+            Authorization:`Bearer ${token}`
+
+          }
+
         }
+
       );
 
-      setReply(response.data.reply);
+      setReply(
 
-    } catch (error) {
+        response.data.response.reply
+
+      );
+
+    }
+
+    catch(error){
+
+      console.log(error);
 
       alert("Chatbot Error");
 
     }
+
   };
 
   return (
+
     <div>
 
       <h2>AI Career Chatbot</h2>
 
       <input
+
         type="text"
+
         placeholder="Ask Question"
-        onChange={(e) => setQuestion(e.target.value)}
+
+        value={question}
+
+        onChange={(e)=>
+
+          setQuestion(
+
+            e.target.value
+
+          )
+
+        }
+
       />
 
       <br /><br />
 
-      <button onClick={askQuestion}>
+      <button
+
+        onClick={askQuestion}
+
+      >
+
         Ask AI
+
       </button>
 
       <br /><br />
 
-      <h3>{reply}</h3>
+      <h3>
+
+        {reply}
+
+      </h3>
 
     </div>
+
   );
+
 }
 
 export default Chatbot;
